abdd19cfe1b0cb414d73fe8bc3b143e227eec25e
AgiHedvig <137860419+AgiHedvig@users.noreply.github.com>
2025-12-23 12:28:14 +0100

    Add files via upload

---
+++ Small Player/Small Player/AssemblyInfo1.cs
@@ -0,0 +1,4 @@
+using MelonLoader;
+using Small_Player;
+[assembly: MelonInfo(typeof(SmallPlayer), "Small Player", "1.0", "AgiHedvig")]
+[assembly: MelonGame("Sad Owl Studios", "Viewfinder")]
---
+++ Small Player/Small Player/SmallPlayer.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using HarmonyLib.Tools;
+using Il2CppDreamteck.Splines.Primitives;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Small_Player
+{
+    public class SmallPlayer : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            GameObject playerObject;
+
+            playerObject = GameObject.FindGameObjectWithTag("Player");
+            playerObject.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+        }
+    }
+}

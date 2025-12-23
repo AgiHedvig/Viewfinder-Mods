566da2c4976c993ea12d3053c391d12587204b7e
AgiHedvig <137860419+AgiHedvig@users.noreply.github.com>
2025-12-23 12:26:15 +0100

    Add files via upload

---
+++ Low Gravity/Low Gravity/AssemblyInfo1.cs
@@ -0,0 +1,4 @@
+using MelonLoader;
+using Low_Gravity;
+[assembly: MelonInfo(typeof(LowGravity), "Low Gravity", "1.0", "AgiHedvig")]
+[assembly: MelonGame("Sad Owl Studios", "Viewfinder")]
---
+++ Low Gravity/Low Gravity/LowGravity.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using HarmonyLib.Tools;
+using Il2Cpp;
+using Il2CppDreamteck.Splines.Primitives;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Low_Gravity
+{
+    public class LowGravity : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            new HarmonyLib.Harmony("com.agihedvig.viewfinder.trainpatch").PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerMovement))]
+    [HarmonyPatch("GetVelocity", MethodType.Normal)]
+    public static class PlayerMovement_Patch
+    {
+        static void Postfix(PlayerMovement __instance)
+        {
+            __instance.Gravity = new Vector3(0, -1, 0);
+        }
+    }
+}

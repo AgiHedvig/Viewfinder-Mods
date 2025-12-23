56954a9be9b6e2e341781b6222824048f4184094
AgiHedvig <137860419+AgiHedvig@users.noreply.github.com>
2025-12-23 12:26:59 +0100

    Add files via upload

---
+++ Rotating Levels/Rotating Levels/AssemblyInfo1.cs
@@ -0,0 +1,4 @@
+using MelonLoader;
+using Rotating_Levels;
+[assembly: MelonInfo(typeof(RotatingLevels), "Rotating Levels", "1.0", "AgiHedvig")]
+[assembly: MelonGame("Sad Owl Studios", "Viewfinder")]
---
+++ Rotating Levels/Rotating Levels/RotatingLevel.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using HarmonyLib.Tools;
+using Il2CppDreamteck.Splines.Primitives;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Rotating_Levels
+{
+    public class RotatingLevels : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            GameObject environment;
+
+            environment = GameObject.Find("Unreferenced Capturable Environment");
+            environment.transform.Rotate(new Vector3(0, 0, 0.02f));
+
+            environment = GameObject.Find("Captureable Environment");
+            environment.transform.Rotate(new Vector3(0, 0, 0.02f));
+
+            environment = GameObject.Find("Non Captureable Environment");
+            environment.transform.Rotate(new Vector3(0, 0, 0.02f));
+        }
+    }
+}

2a82212306f807d73cee0a42f500ec444f709f7b
AgiHedvig <137860419+AgiHedvig@users.noreply.github.com>
2025-12-23 12:23:30 +0100

    Add files via upload

---
+++ Bullet Train/Bullet Train/AssemblyInfo1.cs
@@ -0,0 +1,4 @@
+using MelonLoader;
+using Bullet_Train;
+[assembly: MelonInfo(typeof(BulletTrain), "Bullet Train", "1.0", "AgiHedvig")]
+[assembly: MelonGame("Sad Owl Studios", "Viewfinder")]
---
+++ Bullet Train/Bullet Train/BulletTrain.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using HarmonyLib.Tools;
+using Il2Cpp;
+using Il2CppDreamteck.Splines.Primitives;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Bullet_Train
+{
+    public class BulletTrain : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            new HarmonyLib.Harmony("com.agihedvig.viewfinder.trainpatch").PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(TrainMover))]
+    [HarmonyPatch("ApplyMotion", MethodType.Normal)]
+    public static class TrainMover_ApplyMotion_Patch1
+    {
+        static void Prefix(TrainMover __instance)
+        {
+                __instance.maxSpeed = 50;
+        }
+    }
+
+    [HarmonyPatch(typeof(TrainMover))]
+    [HarmonyPatch("ApplyMotion", MethodType.Normal)]
+    public static class TrainMover_ApplyMotion_Patch2
+    {
+        static void Prefix(TrainMover __instance)
+        {
+                __instance.acceleration = 50;
+        }
+    }
+}
